c8065eeb51b1a770155542c9b7fa6375e3582c09
natounlars <119905870+natounlars@users.noreply.github.com>
2025-12-10 23:06:40 +0800

    Update 50-fdk-aac.sh

--- scripts.d/50-fdk-aac.sh
+++ scripts.d/50-fdk-aac.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-SCRIPT_REPO="https://github.com/mstorsjo/fdk-aac.git"
+SCRIPT_REPO="https://bgithub.xyz/mstorsjo/fdk-aac.git"
 SCRIPT_COMMIT="d8e6b1a3aa606c450241632b64b703f21ea31ce3"
 
 ffbuild_enabled() {
-    [[ $VARIANT == nonfree* ]] || return -1
+    [[ $VARIANT == gpl* ]] || return -1
     return 0
 }
 
